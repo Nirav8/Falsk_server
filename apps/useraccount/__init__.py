6c143d0eb58cf3879409457f11287540ee0951fe
Nirav8 <namoradiya99@outlook.com>
2021-06-02 01:13:01 +0530

    origin

--- apps/useraccount/__init__.py
+++
@@ -1,9 +0,0 @@
-# from flask import session
-# from flask.globals import request
-# from flask import abort
-# from functools import wraps
-
-# def auth_session(f):
-#     @wraps(f)
-#     def inner_function(*args, **kwargs):
-        
